270068ff3be1c233cedcd8e65ae026fdd3d39ba0
Albert-1973 <sdelayshag@gmail.com>
2025-02-16 20:23:22 +0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import json
 from google_sheets import read_data  # Импорт функции для чтения данных
 
 app = Flask(__name__)
@@ -8,9 +9,13 @@ def get_data():
     """Эндпоинт для получения данных из Google Sheets"""
     try:
         data = read_data()  # Читаем данные из таблицы
-        return jsonify(data)  # Flask автоматически обрабатывает кодировку
+        return app.response_class(
+            response=json.dumps({"data": data}, ensure_ascii=False, indent=4),
+            status=200,
+            mimetype="application/json"
+        )
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify(error=str(e)), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
