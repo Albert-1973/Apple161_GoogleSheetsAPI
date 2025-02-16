686b4bbc31af56f922f777116e8548424fbcad5c
Albert-1973 <sdelayshag@gmail.com>
2025-02-16 23:20:35 +0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -45,5 +45,5 @@ def get_products():
 
 if __name__ == "__main__":
     from waitress import serve
-    app.run(host="0.0.0.0", port=10000)
+    serve(app, host="0.0.0.0", port=10000)  # Используем Waitress
 
